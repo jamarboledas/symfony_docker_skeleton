a172c53017c71b60e23f3444ab54f49448b3efc7
jamarboledas <jamarboledas@gmail.com>
2023-06-06 23:12:04 +0200

    feat(testing): hello world test

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
 build-project:
 	docker-compose up -d --build;
 	docker-compose exec php composer install;
+
+tests:
+	docker-compose exec php php ./vendor/bin/phpunit		
---
+++ app/tests/Application/HelloWorldTest.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class HelloWorldTest extends TestCase
+{
+    public function testHello(): void
+    {
+        $this->assertTrue(true);
+    }
+}
